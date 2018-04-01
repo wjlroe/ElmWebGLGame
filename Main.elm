1a4fb456c582270f49bc38ffcc84346ab66476e6
William Roe <git@wjlr.org.uk>
2018-04-01 20:01:05 +0100

    Change to 2 triangles on top of each other

--- Main.elm
+++ Main.elm
@@ -3,23 +3,24 @@ module Main exposing (..)
 import Html exposing (..)
 import Math.Vector2 exposing (..)
 import Math.Vector3 exposing (..)
+import Math.Vector4 exposing (..)
 import WebGL as GL
 
 
 type alias Vertex =
-    { position : Vec2, color : Vec3 }
+    { position : Vec4, color : Vec3 }
 
 
 boxMesh : GL.Mesh Vertex
 boxMesh =
     GL.triangles
-        [ ( Vertex (vec2 -1 1) (vec3 1 0 0)
-          , Vertex (vec2 1 1) (vec3 0 1 0)
-          , Vertex (vec2 -1 -1) (vec3 0 0 1)
+        [ ( Vertex (vec4 0.0 1.0 0.0 1.0) (vec3 1.0 0.0 0.0)
+          , Vertex (vec4 0.5 0.0 0.0 1.0) (vec3 0.0 1.0 0.0)
+          , Vertex (vec4 -0.5 0.0 0.0 1.0) (vec3 0.0 0.0 1.0)
           )
-        , ( Vertex (vec2 -1 -1) (vec3 1 0 0)
-          , Vertex (vec2 1 -1) (vec3 0 1 0)
-          , Vertex (vec2 1 1) (vec3 0 0 1)
+        , ( Vertex (vec4 -0.5 0.0 0.0 1.0) (vec3 0.8 0.2 0.2)
+          , Vertex (vec4 0.5 0.0 0.0 1.0) (vec3 0.2 0.8 0.2)
+          , Vertex (vec4 0.0 -1.0 0.0 1.0) (vec3 0.2 0.2 0.8)
           )
         ]
 
@@ -29,12 +30,12 @@ vertexShader =
     [glsl|
 
 precision mediump float;
-attribute vec2 position;
+attribute vec4 position;
 attribute vec3 color;
 varying vec3 vColor;
 
 void main () {
-  gl_Position = vec4(position, 0.0, 1.0);
+  gl_Position = position;
   vColor = color;
 }
 
